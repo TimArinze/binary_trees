0b5b898cd4b72e545133449f731e0d360c7cf2c9
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-16 16:32:01 +0100

    task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,14 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	else
-	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-		if (left_height > right_height)
-			return (left_height + 1);
-		else
-			return (right_height + 1);
-	}
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
