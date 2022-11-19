a75056b18c3e5fb637e46330c08fe6dec7c0d7b7
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-19 05:45:56 +0100

    task

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - Function that checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 0 if tree is NULL, or 1 if perfect and 0 if not
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left == right)
+		return (1);
+	else
+		return (0);
+}
+/**
+ * binary_tree_height - A function that measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height
+ * Return: 0 if tree is NULL or the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t right_height, left_height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
