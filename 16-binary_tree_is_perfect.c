1e16fa28ca5a1a0e0cefa92a23af11a3a853894a
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-20 20:47:32 +0100

    task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,7 +17,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (left == right)
 	{
 		if (tree->left != NULL && tree->right != NULL)
-			return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+			return (binary_tree_is_perfect(tree->left) &&
+					binary_tree_is_perfect(tree->right));
 		else
 			return (0);
 	}
